aee5b724566802ffb69a059100f7adcda547f2b6
saddamarbaa <saddamarbaas@gmail.com>
2020-12-28 05:17:08 +0700

    Final Build for the Project (jS file)

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,6 @@ function showDiv(element, givenType) {
   const divList = document.getElementsByClassName(element);
   // Iterate over thedivListsList and remove the active class
   for (let i = 0, length = divList.length; i < length; i++) {
-    // hidd the div of index[i]
     divList[i].style.display = "none";
     if (divList[i].classList.contains("active")) {
       divList[i].classList.remove("active");
