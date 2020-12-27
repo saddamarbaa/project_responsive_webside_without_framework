f378b333871c3ddf7a7d4225028555925f638720
saddamarbaa <saddamarbaas@gmail.com>
2020-12-27 23:39:36 +0700

    Done With Js  file

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,17 @@
-function alignText(element, givenType) {
+// function to show the hidden dives
+
+function showDiv(element, givenType) {
   const divList = document.getElementsByClassName(element);
-  // console.log(divList);
   // Iterate over thedivListsList and remove the active class
   for (let i = 0, length = divList.length; i < length; i++) {
+    // hidd the div of index[i]
     divList[i].style.display = "none";
     if (divList[i].classList.contains("active")) {
       divList[i].classList.remove("active");
     }
-    // console.log(divList[i]);
   }
+
   // add the active class only for the current element
-  // document.classList.toggle("active");
   document.getElementById(givenType).classList.toggle("active");
   document.getElementById(givenType).style.display = "block";
 }
